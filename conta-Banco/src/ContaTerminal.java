741bc4db2f914bfaceeefe5d03fc7e190b4928dd
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-05-20 15:20:22 -0300

    Adição do saldo do cliente e ajustes finais

--- conta-Banco/src/ContaTerminal.java
+++ conta-Banco/src/ContaTerminal.java
@@ -10,12 +10,14 @@ public class ContaTerminal {
         //Obter pela classe scanner os valores digitados no terminal
         System.out.print("Digite o seu nome: ");
         String nomeCliente = scanner.next();
-        System.out.print("Por favor, digite o número da Agência !: ");
+        System.out.print("Agora, digite a sua agência: ");
         String agenciaCliente = scanner.next();
-        System.out.print("Agora, digite o número da conta: ");
-        int contaCliente = scanner.nextInt();
+        System.out.print("Por favor, digite o número da Agência !: ");
+        int numeroCliente = scanner.nextInt();
+        System.out.print("Por último, digite o saldo da conta: ");
+        float saldoCliente = scanner.nextFloat();
         // exibir as mensagens para nosso usuário
-        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaCliente + ", conta " + contaCliente + " e seu saldo [Saldo] já está disponível para saque");
+        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaCliente + ", conta " + numeroCliente + " e seu saldo de R$" + saldoCliente + " já está disponível para saque");
         
     }
 }
